2582013508673e6d49d05f3963e1bd9a9298dfd4
heikunKeikun <jjouhansson@hotmail.fi>
2019-05-06 12:01:48 +0300

    added firebase

--- app.js
+++ app.js
@@ -1,149 +1,41 @@
-// nodemon app.js
-
-var bodyParser = require('body-parser');
-var jsonfile = require('jsonfile');
-var express = require('express');
-var app = express();
-
-const file = `${__dirname}\\data.json`;
-
-// extract the entire body portion of an incoming request stream and exposes it on req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-//FIND OUT WHAT THIS REALLY IS!
-app.use(function (req, res, next) {
-    "use strict";
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
+const admin = require('firebase-admin');
+const serviceAccount = require(`${__dirname}\\firebase\\case2019k-firebase-adminsdk-1kfji-4fccf16ecd.json`);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://case2019k.firebaseio.com"
 });
 
-// GET ALL CATEGORIES
-app.get('/category/all', (req, res) => {
-    jsonfile.readFile(file)
-    .then((categories) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(categories));
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+app.use(cors());
+
+app.get('/auth', (req, res) => {
+  let idToken;
+  if (req.headers.token) idToken = req.headers.token;
+
+    // idToken comes from the client app
+  admin.auth().verifyIdToken(idToken)
+  .then((decodedToken) => decodedToken.uid)
+  .then((uid) => {
+    admin.auth().getUser(uid) // get user info with the user id
+    .then(user => {
+      console.log(user);
+      res.writeHead(200, { 'Content-Type': 'application/json' }); // respond with the user id email
+      res.end(JSON.stringify(user.email));
     })
-    .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end();
+    .catch(error => {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(error.message);
     });
-})
-
-// GET CATEGORY BY ID
-app.get('/category/', (req, res) => {
-    const id = req.query.id;
-    const limit = req.query.limit;
-    const above = req.query.above;
-
-    if (id) {
-        jsonfile.readFile(file)
-        .then((categories) => {
-            const response = categories.filter(category => category.id == id);
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(response));
-        })
-        .catch((error) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end(error);
-        })
-    } else if (limit) {
-        if (above === 'true') {
-            jsonfile.readFile(file)
-            .then((categories) => {
-                const response = categories.filter((category) => category.budget > limit);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(response));
-            })
-            .catch((error) => {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end(error);
-            });
-        } else {
-            jsonfile.readFile(file)
-            .then((categories) => {
-                const response = categories.filter((category) => category.budget < limit);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(response));
-            })
-            .catch((error) => {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end(error);
-            });
-        }
-    } else {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end("Reading server side JSON file failed.\n" + "No ID supplied in query.");
-    }
-})
-
-// ADD A NEW CATEGORY
-app.post('/category', (req, res) => {
-    jsonfile.readFile(file)
-    .then((categories) => {
-        let existingCategory = '';
-        existingCategory = categories.find(category => category.id == req.body.id);
-
-        if (existingCategory) {
-            res.writeHead(409, { 'Content-Type': 'text/plain' });
-            res.end('A resource with corresponding ID already exists!');
-            return false;
-        }
-
-        const id = req.body.id;
-        const name = req.body.name;
-        const budget = req.body.budget;
-        const newItem = { id, name, budget };
-
-        categories.push(newItem);
-
-        jsonfile.writeFile(file, categories, { spaces: 2})
-        .then(() => {
-            console.log(categories);
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(JSON.stringify(categories));
-        })
-        .catch((error) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end();
-        });
-    })
-    .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end();
-    });
-});
-
-// DELETE A CATEGORY
-app.delete('/category', (req, res) => {
-    const id = req.query.id;
-    if (id) {
-        jsonfile.readFile(file)
-        .then((categories) => {
-            const newState = categories.filter((category) => category.id != id);
-            jsonfile.writeFile(file, newState, { spaces: 2 })
-            .then(() => {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(JSON.stringify(newState));
-            })
-            .catch((error) => {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end();
-            })
-        })
-        .catch((error) => {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end();
-        })
-    } else {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end("Reading server side JSON file failed.\n" + "No ID supplied in query.");
-    }
+  })
+  .catch((error) => {
+    // Handle error
+    console.log(error.message);
+    res.writeHead(401, { 'Content-Type': 'text/plain' });
+    res.end(error.message);
+  });
 });
 
 // listen port 3001 for connections
